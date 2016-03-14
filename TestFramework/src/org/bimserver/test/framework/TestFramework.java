d10ff1ecf20f6e67fd2beb753bfaea6c66f9db5e
Ruben de Laat <ruben@logic-labs.nl>
2016-03-14 12:47:25 +0100

    changed order

--- TestFramework/src/org/bimserver/test/framework/TestFramework.java
+++ TestFramework/src/org/bimserver/test/framework/TestFramework.java
@@ -110,13 +110,13 @@ public class TestFramework {
 				
 				PluginInterface pluginInterface = bimServer.getService(PluginInterface.class);
 				
+				installPlugin(repository, "org.opensourcebim", "bimserverapi", pluginInterface);
+				installPlugin(repository, "org.opensourcebim", "bimsurfer", pluginInterface);
 				installPlugin(repository, "org.opensourcebim", "ifcplugins", pluginInterface);
 				installPlugin(repository, "org.opensourcebim", "binaryserializers", pluginInterface);
 				installPlugin(repository, "org.opensourcebim", "ifcopenshellplugin", pluginInterface);
 //				installPlugin(repository, "org.opensourcebim", "ifcengine", pluginInterface);
 				installPlugin(repository, "org.opensourcebim", "bimviews", pluginInterface);
-				installPlugin(repository, "org.opensourcebim", "bimsurfer", pluginInterface);
-				installPlugin(repository, "org.opensourcebim", "bimserverapi", pluginInterface);
 			} catch (Exception e) {
 				LOGGER.error("", e);
 			}
