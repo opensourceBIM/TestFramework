93489275d25ca946b46285f5dc6b113fbaddefef
Ruben de Laat <ruben@logic-labs.nl>
2016-03-14 12:45:52 +0100

    test

--- TestFramework/src/org/bimserver/test/framework/TestFramework.java
+++ TestFramework/src/org/bimserver/test/framework/TestFramework.java
@@ -113,7 +113,7 @@ public class TestFramework {
 				installPlugin(repository, "org.opensourcebim", "ifcplugins", pluginInterface);
 				installPlugin(repository, "org.opensourcebim", "binaryserializers", pluginInterface);
 				installPlugin(repository, "org.opensourcebim", "ifcopenshellplugin", pluginInterface);
-				installPlugin(repository, "org.opensourcebim", "ifcengine", pluginInterface);
+//				installPlugin(repository, "org.opensourcebim", "ifcengine", pluginInterface);
 				installPlugin(repository, "org.opensourcebim", "bimviews", pluginInterface);
 				installPlugin(repository, "org.opensourcebim", "bimsurfer", pluginInterface);
 				installPlugin(repository, "org.opensourcebim", "bimserverapi", pluginInterface);
